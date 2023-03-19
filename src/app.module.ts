9a891074cf01e32731d963cd0af8e39f3415f33e
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2023-03-19 17:40:14 -0300

    feat: add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ImcCalculatorModule } from './imc/imc.calculator.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { ImcCalculatorController } from './imc/imc.calculator.ctrl';
 
 @Module({
   imports: [ImcCalculatorModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(ImcCalculatorController);
+  }
+}
